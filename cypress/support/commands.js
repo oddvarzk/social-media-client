6658c9cb233ea19cfb9e5b6a91705f56ef996550
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-04-14 19:58:45 +0200

    cypress commands for login and logout button e2e fixed

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,33 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+//Visits the homepage
+Cypress.Commands.add("visitHomepage", () => {
+  cy.visit("/");
+  cy.wait(500);
+});
+
+// // Logged in is valid
+Cypress.Commands.add("logsIn", () => {
+  cy.get("#loginModal").should("be.visible");
+  cy.get("#loginForm").within(() => {
+    cy.get("#loginEmail").type("yawyawyaw@stud.noroff.no");
+    cy.get("#loginPassword").type("Wagwanmaman122");
+
+    cy.get("button[type=submit]").click();
+  });
+
+  cy.wait(2000);
+});
+
+// Logged in as active user
+Cypress.Commands.add("loggedIn", () => {
+  cy.window().then((win) => {
+    const token = win.localStorage.getItem("token");
+    cy.wrap(token).should("not.be.null");
+    cy.wrap(token).should("be.a", "string").and("not.be.empty");
+  });
+
+  cy.get('[data-cy="profileName"]').should("contain", "testaccount");
+});
